719ae9bbdbac45c2cefc09a62b2bf39f4ac9e2fe
Florent Gravin <florent.gravin@camptocamp.com>
2024-01-09 15:06:09 +0100

    feat: init routing

--- apps/datahub/src/app/app.module.ts
+++ apps/datahub/src/app/app.module.ts
@@ -4,15 +4,18 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import {
+  DefaultRouterModule,
   EmbeddedTranslateLoader,
   TRANSLATE_DEFAULT_CONFIG,
-  UiWidgetsModule,
+  UiWidgetsModule
 } from 'geonetwork-ui';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { SearchPageComponent } from './search/search-page/search-page.component';
+import { DatasetPageComponent } from './dataset/dataset-page/dataset-page.component';
 
 @NgModule({
   declarations: [AppComponent],
@@ -40,6 +43,12 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
     EffectsModule.forRoot([]),
     StoreRouterConnectingModule.forRoot(),
     StoreDevtoolsModule.instrument({ logOnly: !isDevMode() }),
+    DefaultRouterModule.forRoot({
+      searchStateId: 'mainSearch',
+      searchRouteComponent: SearchPageComponent,
+      recordRouteComponent: DatasetPageComponent,
+    }),
+
   ],
   providers: [],
   bootstrap: [AppComponent],
