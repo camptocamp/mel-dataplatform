272a12d4478480ac3c43aef93344466e3e432294
Florent Gravin <florent.gravin@camptocamp.com>
2024-01-09 22:16:38 +0100

    chore(tw): add gnui preset

--- apps/datahub/tailwind.config.js
+++ apps/datahub/tailwind.config.js
@@ -1,15 +1,22 @@
+const baseConfig = require('../../node_modules/geonetwork-ui/tailwind.base.config');
 const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
 const { join } = require('path');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [baseConfig],
   content: [
     './node_modules/geonetwork-ui/**/*.mjs',
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#E30513',
+        beige: '#F7F5F0',
+      },
+    },
   },
   plugins: [],
 };
