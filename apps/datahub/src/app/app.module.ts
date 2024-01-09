b8566afedefea5cc3b7f67f3c9214761eb3bfe3a
Florent Gravin <florent.gravin@camptocamp.com>
2024-01-09 13:57:45 +0100

    chore: configure ngx-translate

--- apps/datahub/src/app/app.module.ts
+++ apps/datahub/src/app/app.module.ts
@@ -3,11 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
-import { UiWidgetsModule } from 'geonetwork-ui';
+import {
+  EmbeddedTranslateLoader,
+  TRANSLATE_DEFAULT_CONFIG,
+  UiWidgetsModule,
+} from 'geonetwork-ui';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, UiWidgetsModule, RouterModule.forRoot(appRoutes)],
+  imports: [
+    BrowserModule,
+    UiWidgetsModule,
+    RouterModule.forRoot(appRoutes),
+    TranslateModule.forRoot({
+      ...TRANSLATE_DEFAULT_CONFIG,
+      loader: {
+        provide: TranslateLoader,
+        useClass: EmbeddedTranslateLoader,
+      },
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
