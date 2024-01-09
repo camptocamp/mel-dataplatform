ed05db4f9847279025c2efd67f6f8ada6e59fcd2
Florent Gravin <florent.gravin@camptocamp.com>
2024-01-09 13:47:53 +0100

    chore: add a random gnui ui component

--- apps/datahub/src/app/app.module.ts
+++ apps/datahub/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { UiWidgetsModule } from 'geonetwork-ui';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
+  declarations: [AppComponent],
+  imports: [BrowserModule, UiWidgetsModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent],
 })
