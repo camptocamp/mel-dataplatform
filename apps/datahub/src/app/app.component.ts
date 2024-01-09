f8c689a89b60a832677bca5916ef887196061787
Florent Gravin <florent.gravin@camptocamp.com>
2024-01-09 13:35:50 +0100

    Initial commit

---
+++ apps/datahub/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mel-dataplatform-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'datahub';
+}
