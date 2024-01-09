8681e55f130ddef2ca945271ed1d105a861feab0
Florent Gravin <florent.gravin@camptocamp.com>
2024-01-09 14:24:22 +0100

    chore(tw): add geonetwork-ui to content

---
+++ apps/datahub/tailwind.config.js
@@ -0,0 +1,15 @@
+const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './node_modules/geonetwork-ui/**/*.mjs',
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
