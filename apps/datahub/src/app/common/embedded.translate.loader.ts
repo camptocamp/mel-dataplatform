7fa0a9e31cbd12e7287825382e544167047ba155
Tobias Kohr <tobias.kohr@camptocamp.com>
2024-01-17 17:33:38 +0100

    chore(package): update gn-ui and use dropEmptyTranslations() from it

--- apps/datahub/src/app/common/embedded.translate.loader.ts
+++ apps/datahub/src/app/common/embedded.translate.loader.ts
@@ -4,19 +4,7 @@ import en_MEL from '../../assets/i18n/en_MEL.json'
 import fr_MEL from '../../assets/i18n/fr_MEL.json'
 import en from 'node_modules/geonetwork-ui/translations/en.json'
 import fr from 'node_modules/geonetwork-ui/translations/fr.json'
-
-//TODO: export and use function from gn-ui
-function dropEmptyTranslations(translations: any) {
-  // filter out empty keys: this should let us fallback on the default lang or
-  // untranslated key, instead of having a blank space
-  return Object.keys(translations).reduce(
-    (prev, curr) =>
-      translations[curr].trim().length
-        ? { ...prev, [curr]: translations[curr] }
-        : prev,
-    {}
-  )
-}
+import { dropEmptyTranslations } from 'geonetwork-ui'
 
 export class MelEmbeddedTranslateLoader implements TranslateLoader {
   getTranslation(lang: string): Observable<any> {
