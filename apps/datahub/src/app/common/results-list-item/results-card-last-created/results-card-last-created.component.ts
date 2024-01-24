3ad612f9ee8efb8ea7f46831dbbec8c36e48568f
Angelika Kinas <angelika.kinas@camptocamp.com>
2024-01-24 13:04:52 +0100

    feat(datahub): Add card for last created result, WIP

--- apps/datahub/src/app/common/results-list-item/results-card-last-created/results-card-last-created.component.ts
+++ apps/datahub/src/app/common/results-list-item/results-card-last-created/results-card-last-created.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  OnInit,
+  Output,
+} from '@angular/core'
 import { ResultsListItemComponent } from '../results-list-item.component'
 
 @Component({
@@ -7,4 +13,25 @@ import { ResultsListItemComponent } from '../results-list-item.component'
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ResultsCardLastCreatedComponent extends ResultsListItemComponent {}
+export class ResultsCardLastCreatedComponent
+  extends ResultsListItemComponent
+  implements OnInit
+{
+  @Output() keyword = new EventEmitter<string>()
+  ngOnInit(): void {
+    console.log(this.record)
+    console.log('creationDate', this.creationDate)
+  }
+
+  get shownOrganization() {
+    return this.record?.ownerOrganization
+  }
+
+  get creationDate() {
+    return this.record?.recordCreated?.toLocaleDateString('fr')
+  }
+
+  onKeywordClick(keyword: string) {
+    this.keyword.emit(keyword)
+  }
+}
--- apps/datahub/src/app/common/results-list/results-list-grid/results-list-grid.component.ts
+++ apps/datahub/src/app/common/results-list/results-list-grid/results-list-grid.component.ts
@@ -1,10 +1,20 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
-import { ResultsListComponent } from '../results-list.component'
-
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
+import { SearchFacade, SearchService } from 'geonetwork-ui'
 @Component({
   selector: 'mel-datahub-results-list-grid',
   templateUrl: './results-list-grid.component.html',
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ResultsListGridComponent extends ResultsListComponent {}
+export class ResultsListGridComponent {
+  @Input() numberOfResults: number
+
+  constructor(
+    private searchService: SearchService,
+    public searchFacade: SearchFacade
+  ) {}
+
+  onInfoKeywordClick(keyword: string) {
+    this.searchService.updateFilters({ any: keyword })
+  }
+}
