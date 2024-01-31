d9a0880052ce0a64daadcce027941ba6b13664f2
Angelika Kinas <angelika.kinas@camptocamp.com>
2024-01-31 16:50:08 +0100

    feat(DH): Add e2e tests for favorite card

--- apps/datahub-e2e/src/e2e/search.cy.ts
+++ apps/datahub-e2e/src/e2e/search.cy.ts
@@ -8,13 +8,36 @@ describe('datahub-e2e', () => {
     cy.get('gn-ui-results-hits-number').should('contain', 12)
   })
   it('should display record results in preview cards', () => {
-    cy.get('mel-datahub-results-list-grid')
-      .find('mel-datahub-results-card-search')
+    cy.get('mel-datahub-results-card-favorite')
       .eq(0)
       .find('h1')
       .should(
         'have.text',
         ' Cartographie des sols agricoles de la plaine du Rhône '
       )
+
+    cy.get('mel-datahub-results-card-favorite')
+      .find('.mel-badge-button')
+      .should('be.visible')
+
+    cy.get('mel-datahub-results-card-favorite')
+      .find('gn-ui-star-toggle')
+      .should('be.visible')
+
+    cy.get('mel-datahub-results-card-favorite')
+      .find('mel-datahub-mel-datahub-metadata-quality')
+      .should('be.visible')
+  })
+
+  describe('interactions with dataset', () => {
+    beforeEach(() => {
+      cy.get('mel-datahub-results-card-favorite').first().as('firstResult')
+    })
+    it('should open the dataset page in the same application on click', () => {
+      const urlRegex = /http:\/\/[^/]+:\d+\/dataset/
+      cy.get('@firstResult').click()
+      cy.url().should('match', urlRegex)
+      cy.get('mel-datahub-dataset-page').should('be.visible')
+    })
   })
 })
