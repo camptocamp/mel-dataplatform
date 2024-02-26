9c6e1e49b79f1bcc60ed8bc91ecd1177829d8a8d
Angelika Kinas <angelika.kinas@camptocamp.com>
2024-02-26 13:46:30 +0100

    chore(homepage): Update gn-ui pacakge for auto- complete component fix, add translation, change styling

--- apps/datahub/src/app/common/autocomplete/autocomplete.component.ts
+++ apps/datahub/src/app/common/autocomplete/autocomplete.component.ts
@@ -1,6 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { AutocompleteComponent } from 'geonetwork-ui'
-import { take } from 'rxjs'
 
 @Component({
   selector: 'mel-datahub-autocomplete',
@@ -8,18 +7,4 @@ import { take } from 'rxjs'
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MelAutocompleteComponent extends AutocompleteComponent {
-  override clear(): void {
-    this.inputRef.nativeElement.value = ''
-    // this.inputCleared.emit()
-    this.selectionSubject
-      .pipe(take(1))
-      .subscribe((selection) => selection && selection.option.deselect())
-    this.inputRef.nativeElement.focus()
-    this.triggerRef.closePanel()
-  }
-  handleEnterFromEvent(event: Event) {
-    const inputValue = (event.target as HTMLInputElement).value
-    this.handleEnter(inputValue)
-  }
-}
+export class MelAutocompleteComponent extends AutocompleteComponent {}
