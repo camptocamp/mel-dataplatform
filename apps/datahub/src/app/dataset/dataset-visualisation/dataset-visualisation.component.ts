735494df4b71a2ab641b076565d7e972e26c8c70
Camille Moinier <cmoinier@wrk208.wrk.prs.camptocamp.com>
2024-03-07 10:01:54 +0100

    feat(css): improve tabs styling

--- apps/datahub/src/app/dataset/dataset-visualisation/dataset-visualisation.component.ts
+++ apps/datahub/src/app/dataset/dataset-visualisation/dataset-visualisation.component.ts
@@ -1,5 +1,8 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
-import { BehaviorSubject } from 'rxjs'
+import { getLinkPriority, getLinkLabel } from 'geonetwork-ui'
+import { BehaviorSubject, combineLatest, map, tap } from 'rxjs'
+import { MdViewFacade } from 'geonetwork-ui'
+import { DatasetDistribution } from 'geonetwork-ui/libs/common/domain/src/lib/model/record'
 
 @Component({
   selector: 'mel-datahub-dataset-visualisation',
@@ -11,6 +14,35 @@ export class DatasetVisualisationComponent {
   @Input() displayMap: boolean
   @Input() displayData: boolean
   selectedTabIndex$ = new BehaviorSubject(0)
+
+  compatibleDataLinks$ = combineLatest([
+    this.mdViewFacade.dataLinks$,
+    this.mdViewFacade.geoDataLinks$,
+  ]).pipe(
+    map(([dataLinks, geoDataLinks]) => {
+      const a = [...dataLinks, ...geoDataLinks]
+      a.sort((a, b) => getLinkPriority(b) - getLinkPriority(a))
+      return a
+    })
+  )
+
+  dropdownChoices$ = this.compatibleDataLinks$.pipe(
+    tap((links) => {
+      if (links.indexOf(this.selectedLink$.value) === -1) {
+        this.selectedLink$.next(links[0])
+      }
+    }),
+    map((links) =>
+      links.map((link) => ({
+        label: getLinkLabel(link),
+        value: JSON.stringify(link),
+      }))
+    )
+  )
+
+  selectedLink$ = new BehaviorSubject<DatasetDistribution>(null)
+
+  constructor(public mdViewFacade: MdViewFacade) {}
 
   onTabIndexChange(index: number): void {
     this.selectedTabIndex$.next(index)
