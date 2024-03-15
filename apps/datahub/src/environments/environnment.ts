3215d050bd32541044fb26c2ccc8e25301aa192e
Camille Moinier <cmoinier@wrk208.wrk.prs.camptocamp.com>
2024-03-15 13:11:04 +0100

    feat(env): get gn-ui version from node-modules

--- apps/datahub/src/environments/environnment.ts
+++ apps/datahub/src/environments/environnment.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @nx/enforce-module-boundaries
-import packageJson from '../../../../package.json'
+import packageJson from 'geonetwork-ui/package.json'
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
@@ -7,7 +7,7 @@ import packageJson from '../../../../package.json'
 export const environment = {
   production: false,
   version:
-    packageJson.dependencies['geonetwork-ui'].split('-')[1] === 'dev'
+    packageJson.version.split('-')[1] === 'dev'
       ? 'main'
-      : `v${packageJson.dependencies['geonetwork-ui']}`,
+      : `v${packageJson.version.split('-')[0]}`,
 }
