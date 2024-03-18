cf4814547c174f4c10c8d97e39dc3e07b1278fb5
Tobias Kohr <tobias.kohr@camptocamp.com>
2024-03-18 17:15:30 +0100

    test(dataset): try to fix and restructure e2e test

--- apps/datahub-e2e/src/e2e/dataset.cy.ts
+++ apps/datahub-e2e/src/e2e/dataset.cy.ts
@@ -34,7 +34,7 @@ describe('datasets', () => {
     })
 
     it('should scroll down when clicking on download button', () => {
-      cy.get('mel-datahub-button').eq(1).as('downloadButton')
+      cy.get('mel-datahub-button').eq(2).as('downloadButton')
       cy.get('@downloadButton').click()
       cy.get('@downloadButton').should(() => {
         const scrollPosition = Cypress.dom.getWindowByElement(
@@ -45,123 +45,13 @@ describe('datasets', () => {
     })
 
     it('should scroll down when clicking on api button', () => {
-      cy.get('mel-datahub-button').eq(2).as('apiButton')
+      cy.get('mel-datahub-button').eq(3).as('apiButton')
       cy.get('@apiButton').click()
       cy.get('@apiButton').should(() => {
         const scrollPosition = Cypress.dom.getWindowByElement(
           cy.state('window')
         ).scrollY
         expect(scrollPosition).to.be.greaterThan(0)
-      })
-    })
-
-    describe('Information block', () => {
-      beforeEach(() => {
-        cy.visit('/dataset/ee965118-2416-4d48-b07e-bbc696f002c2')
-        cy.get('mel-datahub-dataset-information')
-          .children('div')
-          .first()
-          .children('div')
-          .eq(1)
-          .as('mainInfo')
-      })
-      it('should display the information block', () => {
-        cy.get('mel-datahub-dataset-information').should('be.visible')
-      })
-
-      it('should display the update date', () => {
-        cy.get('@mainInfo')
-          .first()
-          .find('div')
-          .first()
-          .find('span')
-          .should('have.text', 'Mise à jour le')
-      })
-
-      it('should display the categories btns', () => {
-        cy.get('@mainInfo')
-          .children('div')
-          .eq(1)
-          .find('div')
-          .children('span')
-          .should('have.length.gt', 0)
-      })
-
-      it('should display the territories', () => {
-        cy.get('@mainInfo')
-          .children('div')
-          .eq(2)
-          .find('div')
-          .children('span')
-          .should('have.length.gt', 0)
-      })
-
-      it('should display the licenses', () => {
-        cy.get('@mainInfo')
-          .children('div')
-          .eq(3)
-          .children('span')
-          .eq(1)
-          .should('have.length.gt', 0)
-      })
-
-      it('should display the producer', () => {
-        cy.get('@mainInfo')
-          .children('div')
-          .eq(4)
-          .find('span')
-          .eq(1)
-          .should('have.text', 'Région Hauts-de-France')
-      })
-
-      it('should display the social media share btns', () => {
-        cy.get('mel-datahub-dataset-information')
-          .children('div')
-          .first()
-          .children('div')
-          .eq(2)
-          .children('div')
-          .first()
-          .find('a')
-          .should('have.length', 4)
-      })
-    })
-
-    describe('API block', () => {
-      beforeEach(() =>
-        cy.visit('/dataset/ee965118-2416-4d48-b07e-bbc696f002c2')
-      )
-
-      it('should display the API block', () => {
-        cy.get('mel-datahub-dataset-apis').should('be.visible')
-      })
-      it('should have API cards', () => {
-        cy.get('mel-datahub-dataset-apis')
-          .find('mel-datahub-custom-carousel')
-          .find('mel-datahub-api-card')
-          .should('have.length.gt', 0)
-      })
-      it('should display the swagger link', () => {
-        cy.get('mel-datahub-dataset-apis')
-          .find('mel-datahub-custom-carousel')
-          .find('mel-datahub-api-card')
-          .last()
-          .click()
-        cy.get('mel-datahub-api-form')
-          .find('a')
-          .invoke('attr', 'href')
-          .should(
-            'eq',
-            'https://mel.integration.apps.gs-fr-prod.camptocamp.com/data/swagger-ui/index.html'
-          )
-      })
-      it('should open the api form', () => {
-        cy.get('mel-datahub-dataset-apis')
-          .find('mel-datahub-custom-carousel')
-          .find('mel-datahub-api-card')
-          .last()
-          .click()
-        cy.get('mel-datahub-api-form').should('be.visible')
       })
     })
 
@@ -184,41 +74,149 @@ describe('datasets', () => {
         .and('satisfy', (maxHeight) => parseInt(maxHeight, 10) > 110)
     })
 
-    describe('Downloads section', () => {
-      beforeEach(() => {
-        cy.visit('/dataset/04bcec79-5b25-4b16-b635-73115f7456e4')
-      })
-      it('should download button should contain the correct link and open in new tab', () => {
-        cy.get('[data-cy="download-button"]')
-          .first()
-          .invoke('attr', 'href')
-          .as('downloadLink')
-        cy.get('@downloadLink').should(
-          'contain',
-          '/geoserver/insee/ows?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=insee%3Arectangles_200m_menage_erbm&OUTPUTFORMAT=csv'
-        )
-        cy.get('[data-cy="download-button"]')
-          .first()
-          .should('have.attr', 'target', '_blank')
-      })
-      it('should copy the link resource to clipboard when clicking on copy button', () => {
-        cy.get('body').focus()
-        cy.get('[data-cy="copy-button"]').first().click()
-        // attempt to make the whole page focused
-        cy.get('body').focus()
-
-        cy.window().then((win) => {
-          win.navigator.clipboard.readText().then((text) => {
-            expect(text).to.eq(
-              'https://www.geo2france.fr/geoserver/insee/ows?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=insee%3Arectangles_200m_menage_erbm&OUTPUTFORMAT=csv'
-            )
-          })
-        })
-      })
-    })
-
     it('should display the footer', () => {
       cy.get('mel-datahub-footer').should('be.visible')
+    })
+  })
+
+  describe('Information block', () => {
+    beforeEach(() => {
+      cy.visit('/dataset/ee965118-2416-4d48-b07e-bbc696f002c2')
+      cy.get('mel-datahub-dataset-information')
+        .children('div')
+        .first()
+        .children('div')
+        .eq(1)
+        .as('mainInfo')
+    })
+    it('should display the information block', () => {
+      cy.get('mel-datahub-dataset-information').should('be.visible')
+    })
+
+    it('should display the update date', () => {
+      cy.get('@mainInfo')
+        .first()
+        .find('div')
+        .first()
+        .find('span')
+        .should('have.text', 'Mise à jour le')
+    })
+
+    it('should display the categories btns', () => {
+      cy.get('@mainInfo')
+        .children('div')
+        .eq(1)
+        .find('div')
+        .children('span')
+        .should('have.length.gt', 0)
+    })
+
+    it('should display the territories', () => {
+      cy.get('@mainInfo')
+        .children('div')
+        .eq(2)
+        .find('div')
+        .children('span')
+        .should('have.length.gt', 0)
+    })
+
+    it('should display the licenses', () => {
+      cy.get('@mainInfo')
+        .children('div')
+        .eq(3)
+        .children('span')
+        .eq(1)
+        .should('have.length.gt', 0)
+    })
+
+    it('should display the producer', () => {
+      cy.get('@mainInfo')
+        .children('div')
+        .eq(4)
+        .find('span')
+        .eq(1)
+        .should('have.text', 'Région Hauts-de-France')
+    })
+
+    it('should display the social media share btns', () => {
+      cy.get('mel-datahub-dataset-information')
+        .children('div')
+        .first()
+        .children('div')
+        .eq(2)
+        .children('div')
+        .first()
+        .find('a')
+        .should('have.length', 4)
+    })
+  })
+
+  describe('API block', () => {
+    beforeEach(() => cy.visit('/dataset/ee965118-2416-4d48-b07e-bbc696f002c2'))
+
+    it('should display the API block', () => {
+      cy.get('mel-datahub-dataset-apis').should('be.visible')
+    })
+    it('should have API cards', () => {
+      cy.get('mel-datahub-dataset-apis')
+        .find('mel-datahub-custom-carousel')
+        .find('mel-datahub-api-card')
+        .should('have.length.gt', 0)
+    })
+    it('should display the swagger link', () => {
+      cy.get('mel-datahub-dataset-apis')
+        .find('mel-datahub-custom-carousel')
+        .find('mel-datahub-api-card')
+        .last()
+        .click()
+      cy.get('mel-datahub-api-form')
+        .find('a')
+        .invoke('attr', 'href')
+        .should(
+          'eq',
+          'https://mel.integration.apps.gs-fr-prod.camptocamp.com/data/swagger-ui/index.html'
+        )
+    })
+    it('should open the api form', () => {
+      cy.get('mel-datahub-dataset-apis')
+        .find('mel-datahub-custom-carousel')
+        .find('mel-datahub-api-card')
+        .last()
+        .click()
+      cy.get('mel-datahub-api-form').should('be.visible')
+    })
+  })
+
+  describe('Downloads section', () => {
+    beforeEach(() => {
+      cy.visit('/dataset/04bcec79-5b25-4b16-b635-73115f7456e4')
+    })
+    it('should download button should contain the correct link and open in new tab', () => {
+      cy.get('[data-cy="download-button"]')
+        .first()
+        .invoke('attr', 'href')
+        .as('downloadLink')
+      cy.get('@downloadLink').should(
+        'contain',
+        '/geoserver/insee/ows?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=insee%3Arectangles_200m_menage_erbm&OUTPUTFORMAT=csv'
+      )
+      cy.get('[data-cy="download-button"]')
+        .first()
+        .should('have.attr', 'target', '_blank')
+    })
+    it('should copy the link resource to clipboard when clicking on copy button', () => {
+      cy.get('body').focus()
+      cy.get('[data-cy="copy-button"]').first().click()
+      // attempt to make the whole page focused
+      cy.get('body').focus()
+
+      cy.window().then((win) => {
+        win.navigator.clipboard.readText().then((text) => {
+          expect(text).to.eq(
+            'https://www.geo2france.fr/geoserver/insee/ows?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=insee%3Arectangles_200m_menage_erbm&OUTPUTFORMAT=csv'
+          )
+        })
+      })
     })
   })
 
