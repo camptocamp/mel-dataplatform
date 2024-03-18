47fd196a9a87b45430e272f3c89552c0a1245c47
Camille Moinier <cmoinier@wrk208.wrk.prs.camptocamp.com>
2024-03-18 10:48:08 +0100

    fix(e2e): fix e2e tests

--- apps/datahub/src/environments/environnment.ts
+++ apps/datahub/src/environments/environnment.ts
@@ -6,8 +6,7 @@ import packageJson from 'geonetwork-ui/package.json'
 
 export const environment = {
   production: false,
-  version:
-    packageJson.version.split('-')[1].includes('dev')
-      ? 'main'
-      : `v${packageJson.version.split('-')[0]}`,
+  version: packageJson.version.split('-')[1].includes('dev')
+    ? 'main'
+    : `v${packageJson.version.split('-')[0]}`,
 }
