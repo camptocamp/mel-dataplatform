7e884aaa494a6c4dd9c36f73a714e0d25e3cbfc1
Camille Moinier <cmoinier@wrk208.wrk.prs.camptocamp.com>
2024-05-23 16:34:30 +0200

    feat: add mention-legales footer for versioning

---
+++ old/mentions-legales-v1/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'gray-2': '#4C4C4C',
+      },
+    },
+  },
+  plugins: [],
+}
