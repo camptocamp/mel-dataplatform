73cd07d3ea5353306f8263c6c02afa9898923321
Tobias Kohr <tobias.kohr@camptocamp.com>
2024-12-18 10:03:20 +0100

    fix(download-item): add filename with extension to download attribute
    for json and geojson files. other file formats should have a name and don't need download attribute. note that download attribute only takes effect on same-origin

--- apps/datahub/src/app/dataset/dataset-downloads/download-item/download-item.component.ts
+++ apps/datahub/src/app/dataset/dataset-downloads/download-item/download-item.component.ts
@@ -12,4 +12,15 @@ export class MelDownloadItemComponent extends DownloadItemComponent {
     navigator.clipboard.writeText(this.link.url.href)
     ;(event.target as HTMLElement).blur()
   }
+
+  // note that the download attribute calling this getter only takes effect on same-origin resources
+  get downloadFileName() {
+    let fileName = ''
+    if (this.format === 'geojson') {
+      fileName = 'data.geojson'
+    } else if (this.format === 'json') {
+      fileName = 'data.json'
+    }
+    return fileName
+  }
 }
