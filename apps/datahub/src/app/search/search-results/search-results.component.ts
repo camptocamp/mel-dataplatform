4420cf41707350b4cc669f62d06227c86eaa04dd
Tobias Kohr <tobias.kohr@camptocamp.com>
2025-01-16 16:18:06 +0100

    chore(search-results): migrate pagination

--- apps/datahub/src/app/search/search-results/search-results.component.ts
+++ apps/datahub/src/app/search/search-results/search-results.component.ts
@@ -9,15 +9,42 @@ import { SearchFacade, SearchService } from 'geonetwork-ui'
 })
 export class SearchResultsComponent {
   pageSize = 18
+  totalPages_: number
+  currentPage_: number
 
   constructor(
     protected searchFacade: SearchFacade,
     protected searchService: SearchService
   ) {
     this.searchFacade.setPageSize(this.pageSize)
+    this.searchFacade.currentPage$.subscribe((page) => {
+      this.currentPage_ = page
+    })
+    this.searchFacade.totalPages$.subscribe((total) => {
+      this.totalPages_ = total
+    })
   }
 
-  onPageChange(page: number) {
+  // Paginable API
+  get isFirstPage() {
+    return this.currentPage_ === 1
+  }
+  get isLastPage() {
+    return this.currentPage_ === this.totalPages_
+  }
+  get pagesCount() {
+    return this.totalPages_
+  }
+  get currentPage() {
+    return this.currentPage_
+  }
+  goToPage(page: number) {
     this.searchService.setPage(page)
+  }
+  goToNextPage() {
+    this.searchService.setPage(this.currentPage_ + 1)
+  }
+  goToPrevPage() {
+    this.searchService.setPage(this.currentPage_ - 1)
   }
 }
