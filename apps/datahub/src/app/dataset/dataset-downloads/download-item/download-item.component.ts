e598b04944c7fdbdb13883e78095b08dca48b10a
ronitjadhav <ronit.jadhav@camptocamp.com>
2025-02-18 14:25:06 +0100

    move format colors to constants file and update imports

--- apps/datahub/src/app/dataset/dataset-downloads/download-item/download-item.component.ts
+++ apps/datahub/src/app/dataset/dataset-downloads/download-item/download-item.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { DownloadItemComponent } from 'geonetwork-ui'
+import { formatColors } from '@mel-dataplatform/mel'
 
 @Component({
   selector: 'mel-datahub-download-item',
@@ -26,40 +27,12 @@ export class MelDownloadItemComponent extends DownloadItemComponent {
   }
 
   getBadgeStyles(format: string): { [key: string]: string } {
-    const badgeColors: { [key: string]: string } = {
-      csv: '#F6A924',
-      excel: '#FFDE10',
-      gpkg: '#7D5D9F',
-      shp: '#009036',
-      gml: '#E75113',
-      geojson: '#293C6F',
-      json: '#84D0F0',
-      zip: '#B0CB52',
-      dxf: '#DCCD00',
-      kml: '#F4B5D0',
-      pdf: '#49579E',
-      jpg: '#009EE0',
-      svg: '#EB6D82',
-      html: '#C0C9B6',
-      fgb: '#C4A98F',
-      jsonfg: '#A8111C',
+    const lowerFormat = format?.toLowerCase() || ''
+    const colors = formatColors[lowerFormat] || {
+      background: '#CCCDD2',
+      text: '#000',
     }
 
-    const lowerFormat = format?.toLowerCase() || ''
-    const backgroundColor = badgeColors[lowerFormat] || '#CCCDD2'
-    const textColor = [
-      'gpkg',
-      'shp',
-      'gml',
-      'geojson',
-      'pdf',
-      'jpg',
-      'svg',
-      'jsonfg',
-    ].includes(lowerFormat)
-      ? '#FFF'
-      : '#000'
-
-    return { 'background-color': backgroundColor, color: textColor }
+    return { 'background-color': colors.background, color: colors.text }
   }
 }
